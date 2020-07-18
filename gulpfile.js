751e13d117b33f1c1343fbb1031ed2435ea1b02a
ksukhaldun <kseniya.khaldun@gmail.com>
2020-07-18 14:07:56 +0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -87,3 +87,10 @@ gulp.task('watch', function(){
 gulp.task('build', gulp.series('clean', 'export'))
 
 gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
+
+var ghPages = require('gulp-gh-pages');
+ 
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
